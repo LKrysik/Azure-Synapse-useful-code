12ea61da0d476f1d4bdde5a892f7e8303ba465ba
lkrysik <hergati@gmail.com>
2024-05-13 09:38:16 +0200

    Update Usage.py

--- create mount with Linked Service (spark 3.3)/Usage.py
+++ create mount with Linked Service (spark 3.3)/Usage.py
@@ -1,10 +1,20 @@
-mount_point = mount_from_linkedservice(target_linked_service, target_container)
+from collections import defaultdict
 
-#Get job id to read data with synfs
+# Job id
 job_id = mssparkutils.env.getJobId()
 
-#Spark 3.3 !!! 
-synfs_path = f"synfs:/{job_id}{mount_point}{target_path}/{target_table_name}"
+# Mount points
+mount_point = defaultdict(dict)
 
+linked_service_name = "LINKED_SERVCIE_TO_ADLS_LS"
+container = "raw"
 
+# if a mount point has been declared, do not create it again
+if not mount_point.get(linked_service_name,{}).get(container,None):
+   mount_point[linked_service_name][container] = mount_from_linkedservice(linked_service_name, container)
+
+#spark version 3.3
+synfs_path = f"synfs:/{job_id}{mount_point[linked_service_name][container]}/{path}/"
+
+#Write data
 dataframe.coalesce(1).write.mode("overwrite").partitionBy(*partitionBy).parquet(synfs_path)
